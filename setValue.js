606b6079501efb74384053b7b18ed036104e679d
Ian <modulesQuimera@gmail.com>
2020-07-09 17:46:38 -0400

    update

--- setValue.js
+++ setValue.js
@@ -1,7 +1,6 @@
 module.exports = function(RED) {
 
     "use strict";
-    // var serialPool = require("../../serial-template/serial")
     var mapeamentoNode;
     function setValueNode(config) {
         RED.nodes.createNode(this, config);
@@ -13,75 +12,28 @@ module.exports = function(RED) {
         node.val = config.val
         mapeamentoNode = RED.nodes.getNode(this.mapeamento);
 
-        // if (this.serialConfig) { 
-        //     node.port = serialPool.get(this.serialConfig);
-            node.on('input', function(msg, send, done) {
-                var globalContext = node.context().global;
-                var exportMode = globalContext.get("exportMode");
-                var currentMode = globalContext.get("currentMode");
-                var command = {
-                    type: "GPIO_modular_V1.0",
-                    slot: 1,
-                    method: "set_value",
-                    GPIO_number: parseInt(node.gpio_number),
-                    GPIO_value: node.val === "true" ? true : false
-                }
-                // if(exportMode){
-                    var slot = globalContext.get("slot");
+        node.on('input', function(msg, send, done) {
+            var globalContext = node.context().global;
+            var exportMode = globalContext.get("exportMode");
+            var currentMode = globalContext.get("currentMode");
+            var command = {
+                type: "GPIO_modular_V1.0",
+                slot: 1,
+                method: "set_value",
+                GPIO_number: parseInt(node.gpio_number),
+                GPIO_value: node.val === "true" ? true : false
+            }
+            var slot = globalContext.get("slot");
 
-                    var file = globalContext.get("exportFile")
-                    if(currentMode == "test"){file.slots[slot].jig_test.push(command)}
-                    else{file.slots[slot].jig_error.push(command)}
-                    globalContext.set("exportFile", file);
-                    send(msg)
-                    console.log(command)
-
-                // }
-                // else{
-                //     node.status({fill:"yellow", shape:"dot", text:"waiting"}); // seta o status pra waiting
-    
-                //     node.port.enqueue(msg, node,function(err,res) { // empilha a informacao a ser passada via serial
-                //         if (err) {
-                //             var errmsg = err.toString().replace("Serialport","Serialport " + node.port.serial.path);
-                //             node.error(errmsg,msg);
-                //         }
-                //     });
-                // }
-            });
-            // nao mexa em nada daqui pra baixo
-        //     this.port.on('data', function(msgout, sender) {
-        //         if (sender !== node) { return; }
-        //         node.status({fill:"green",shape:"dot",text:"ok"});
-        //         msgout.status = "OK";
-        //         node.send(msgout);
-        //     });
-        //     this.port.on('timeout', function(msgout, sender) {
-        //         if (sender !== node) { return; }
-        //         msgout.status = "ERR_TIMEOUT";
-        //         node.status({fill:"red",shape:"ring",text:"timeout"});
-        //         node.send(msgout);
-        //     });
-        //     node.port.on('ready', function() {
-        //         node.status({fill:"green",shape:"dot",text:"connected"});
-        //     });
-        //     node.port.on('closed', function() {
-        //         node.status({fill:"red",shape:"ring",text:"not-connected"});
-        //     });
-        // }
-        // else {
-        //     this.error(RED._("serial.errors.missing-conf"));
-        // }
-        // this.on("close", function(done) {
-        //     if (this.serialConfig) {
-        //         serialPool.close(this.serialConfig.serialport,done);
-        //     }
-        //     else {
-        //         done();
-        //     }
-        // });
+            var file = globalContext.get("exportFile")
+            if(currentMode == "test"){file.slots[slot].jig_test.push(command)}
+            else{file.slots[slot].jig_error.push(command)}
+            globalContext.set("exportFile", file);
+            send(msg)
+            console.log(command)
+        });
     }
 
-    // nome do modulo
     RED.nodes.registerType("setValue", setValueNode);
 
     RED.httpAdmin.get("/setValue",function(req,res) {
